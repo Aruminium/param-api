7b71cb4ec814a463867acaf01a534ab2a0468099
Aruminium <b2201680@photon.chitose.ac.jp>
2022-10-21 21:59:23 +0900

    add jwt options

--- app/api/auth/oauth2.py
+++ app/api/auth/oauth2.py
@@ -25,7 +25,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
   else:
     expire = datetime.utcnow() + timedelta(minutes=15)
   to_encode.update({"exp": expire})
-  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM, options={"verify_signature": False})
+  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
 
 async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
